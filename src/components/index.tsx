8a13ce941fd5429e545c0385f5bd15a6a2a9f0c0
Adoobdoob71 <eladelad71@gmail.com>
2021-04-07 21:29:43 +0300

    📦 NEW: Added navigation

---
+++ src/components/index.tsx
@@ -0,0 +1,8 @@
+export { default as Row } from "./Row/Row";
+export { default as Column } from "./Column/Column";
+
+export { default as IconButton } from "./IconButton/IconButton";
+export { default as Button } from "./Button/Button";
+
+export { default as Header } from "./Header/Header";
+export { default as Alert } from "./Alert/Alert";
