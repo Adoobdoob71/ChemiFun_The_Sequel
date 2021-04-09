4f46f31ae39b24cb1e0724ff4558a738d3737fe8
Adoobdoob71 <eladelad71@gmail.com>
2021-04-09 11:25:32 +0300

    📦 NEW: Participant component added

---
+++ src/components/Participant/Participant.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Participant.css";
+
+interface ParticipantProps {
+  nickname: string;
+  leader: boolean;
+  points: number;
+}
+
+const Participant: React.FC<ParticipantProps> = (props) => {
+  return (
+    <div
+      className={`participant_main_view flex flex_direction_row align_center ${
+        props.leader ? "" : "participant_leader"
+      }`}>
+      <span className="participant_nickname font_medium">{props.nickname}</span>
+      <span className="participant_points font_small">{props.points}/15</span>
+    </div>
+  );
+};
+
+export default Participant;
