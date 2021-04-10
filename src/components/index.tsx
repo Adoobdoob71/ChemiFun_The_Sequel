966e8ca7b6653f5568290e4ae833ce5faf70cdae
Adoobdoob71 <eladelad71@gmail.com>
2021-04-10 13:59:31 +0300

    👌 IMPROVE: Improved file structure

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,5 @@
-export { default as Row } from "./Row/Row";
-export { default as Column } from "./Column/Column";
+export { default as Row } from "./Row";
+export { default as Column } from "./Column";
 
 export { default as IconButton } from "./IconButton/IconButton";
 export { default as Button } from "./Button/Button";
